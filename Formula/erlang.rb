8a4dbcde991fc5cdf36399be5c53ab9be0fe014f
Aladdin Koraiem <alaadahmed@gmail.com>
2021-11-22 11:14:07 +0200

    resource block updated

--- Formula/erlang.rb
+++ Formula/erlang.rb
@@ -32,9 +32,9 @@ class Erlang < Formula
   depends_on "wxwidgets" # for GUI apps like observer
 
   resource "html" do
-    url "https://github.com/erlang/otp/releases/download/OTP-24.1.6/otp_doc_html_24.1.6.tar.gz"
-    mirror "https://fossies.org/linux/misc/otp_doc_html_24.1.6.tar.gz"
-    sha256 "2b96a49b30f0402681acd8522d7cec6530d9b40c28067005d9bc633125ea7135"
+    url "https://github.com/erlang/otp/releases/download/OTP-24.1.7/otp_doc_html_24.1.7.tar.gz"
+    mirror "https://fossies.org/linux/misc/otp_doc_html_24.1.7.tar.gz"
+    sha256 "cd5ac7007fec0847cf84589e6d0f5055578df0f96edc13379abe579ecd5d17d7"
   end
 
   def install
